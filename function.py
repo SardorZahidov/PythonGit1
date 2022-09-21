985fd64194bbc41ccfb145c4d7474cc71c6fdc89
SardorZahidov <szahidov575@gmail.com>
2022-09-21 16:39:18 +0300

    first commit

---
+++ function.py
@@ -0,0 +1,17 @@
+# import main
+
+# main.boom()
+
+########################
+
+#import main
+
+# main.chat_bot()
+
+############################
+
+# import main
+
+# main.shor_word("Кыргызская Ресрублика") 
+# main.shor_word("Ruby on Rails")
+# main.shor_word("For your interest") 
---
+++ main.py
@@ -0,0 +1,31 @@
+# Задача1
+
+# def boom():
+#     lst = [1, 2, 3, 4 ,5, 6, 7, 8, 9, 10]
+#     lst.reverse()
+#     print(lst)
+
+#Задача2
+
+# def chat_bot():
+#     while True:
+#         word = input("Напишите слово:")
+#         if word =="":
+#             print("Как классно, когда ты молчишь. Продолжай в том же духе!")
+        # elif "!" in word:
+        #     print("Успокойся")
+        # elif "?" in word:
+        #     print("Конечно!")
+        # else:
+        #     print("Ну и что")
+
+#Задача3
+
+# def shor_word(word): 
+#     word_split = word.split() 
+#     new_lst = [] 
+#     for i in word_split: 
+#         new_lst.append(i.title()[0]) 
+#     res = "".join(new_lst) 
+#     print(res) 
+
